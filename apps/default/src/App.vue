5fabaab60d15a34bb492a4dc2317b4145ecd4bff
Frank <franklee809@gmail.com>
2025-12-01 15:32:56 +0800

    refactor: css to scss

--- apps/default/src/App.vue
+++ apps/default/src/App.vue
@@ -1,84 +1,83 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import ExampleComponent from './components/ExampleComponent.vue'
+import MagicWand from './components/icons/MagicWand.vue'
 </script>
 
 <template>
   <header>
 
     <div class="wrapper">
+      <MagicWand class="logo" />
       <nav>
+
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/example">Example</RouterLink>
       </nav>
     </div>
-    <ExampleComponent />
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 header {
   line-height: 1.5;
   max-height: 100vh;
+
+  @media (min-width: 1024px) {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+
+    .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: nowrap;
+      width: 100%;
+      align-items: center;
+    }
+  }
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
+  flex-basis: 5vw;
+
+  @media (min-width: 1024px) {
+    margin: 0 2rem 0 0;
+  }
 }
 
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+    &.router-link-exact-active {
+      color: var(--color-text);
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+      &:hover {
+        background-color: transparent;
+      }
+    }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    &:first-of-type {
+      border: 0;
+    }
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
+  @media (min-width: 1024px) {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
