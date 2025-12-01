fbf736aa59e7362ba4ff579d48890aafdd49f9a3
Frank <franklee809@gmail.com>
2025-12-01 15:22:02 +0800

    refactor: replace current path to src alias

--- apps/default/src/views/HomeView.vue
+++ apps/default/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ExampleComponent from '@/components/ExampleComponent.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ExampleComponent />
   </main>
 </template>
